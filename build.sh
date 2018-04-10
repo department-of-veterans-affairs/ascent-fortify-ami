50fd1981bc59e6c6f73a4856fe4e97212eb1776e
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-04-10 14:37:03 -0500

    PLAT-147: add download from S3 bucket of fortify zip and license

--- build.sh
+++ build.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-# -- check if aws exists and fail if it doesn't
-type aws >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed. Aborting."; exit 1; }
+echo "Check if aws exists"
+type aws >/dev/null 2>&1 || { printf "\xE2\x9D\x8C   aws cli probably not installed. Aborting..."; exit 1; }
 
-# -- Download Fortify server
-aws s3api get-object --bucket ascent-fortify --key HPE_Security_Fortify_17.20_Server_WAR_Tomcat.zip  Fortify.zip
+echo ""
+if [ ! -f Fortify.zip ]; then
+    echo "Fortify.zip not found. Downloading..."
+    aws s3api get-object --bucket ascent-fortify --key HPE_Security_Fortify_17.20_Server_WAR_Tomcat.zip  Fortify.zip
+    echo "Done."
+fi
+echo ""
 
-# -- Download Fortify license
-aws s3api get-object --bucket ascent-fortify --key fortify.license fortify.license
+if [ ! -f fortify.license ]; then
+    echo "Fortify license not found. Downloading..."
+    aws s3api get-object --bucket ascent-fortify --key fortify.license fortify.license
+    echo "Done."
+fi
 
 
 
-packer build fortify.json
+
+echo "Packer building an ami..."
+packer build -var-file=./settings.json fortify.json
 
