826c691ccf1ae7631615859f7bd0399f48bdea37
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-04-11 18:32:55 -0500

    PLAT-150: add aws-cli and json parser to tools to be installed

--- modules/fortify-server/fortify-user-data.sh
+++ modules/fortify-server/fortify-user-data.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Acquire fortify license from s3 bucket
+aws s3api get-object --bucket ascent-fortify --key fortify.license fortify.license
+sudo mv ./fortify.license /root/.fortify/fortify.license
+
+# The variables below are filled in via Terraform interpolation
 sudo tomcatup
