8da30fc6dd8205f2b23410c39d35784bd0d44b7c
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-04-11 08:55:47 -0500

    PLAT-147: add connector jar to classpath of tomcat

--- install-fortify.sh
+++ install-fortify.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 unzip -d Fortify Fortify.zip
-sudo cp Fortify/ssc.war /opt/tomcat/apache-tomcat-8.5.29/webapps/
+sudo cp Fortify/ssc.war /opt/tomcat/apache-tomcat-8.5.30/webapps/
+
+# Download connector
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.zip
+unzip mysql-connector-java-5.1.46.zip
+chmod 755 mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar
+
+sudo mv /home/ec2-user/setenv.sh /opt/tomcat/apache-tomcat-8.5.30/bin/
+sudo chmod 755 /opt/tomcat/apache-tomcat-8.5.30/bin/setenv.sh
+sleep 30
 sudo tomcatdown
 sleep 30
 sudo tomcatup
