32a9be94ab1609cd0766b279648d44d713958335
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-04-13 05:58:44 -0500

    PLAT-150: add aws cli and jq install to tools

--- packer/install-tools.sh
+++ packer/install-tools.sh
@@ -9,3 +9,11 @@ sudo yum -y install unzip
 # Install wget
 sudo yum -y install wget
 
+# aws cli
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+# jq command line JSON parser
+sudo wget -O /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+sudo chmod 755 /usr/bin/jq
