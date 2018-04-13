796402b896c3b2d8e4b3be7331ea83fd7294a619
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-04-13 05:57:20 -0500

    PLAT-150: add wait for tomcat script execution to install-fortify script so that tomcat can explode the fortify war file

--- packer/install-fortify.sh
+++ packer/install-fortify.sh
@@ -3,14 +3,16 @@
 unzip -d Fortify Fortify.zip
 sudo cp Fortify/ssc.war /opt/tomcat/apache-tomcat-8.5.30/webapps/
 
-# Download connector
+# Download connector and move to appropriate location
 wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.zip
 unzip mysql-connector-java-5.1.46.zip
 chmod 755 mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar
-
 sudo mv /home/ec2-user/setenv.sh /opt/tomcat/apache-tomcat-8.5.30/bin/
 sudo chmod 755 /opt/tomcat/apache-tomcat-8.5.30/bin/setenv.sh
-sleep 30
+
 sudo tomcatdown
-sleep 30
-sudo tomcatup
+sleep 5
+
+# wait for tomcat to be fully up before continuing so that 
+# fortify war file can 'explode'
+sudo /home/ec2-user/wait_for_tomcat_up.sh
