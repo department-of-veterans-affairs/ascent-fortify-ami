1ed6bc5c97e581356364a17eaaeb24575b1b480b
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-04-30 10:27:56 -0500

    PLAT-179: have run-sca and run-register-jenkins-node for the user data for jenkins-fortify-slave

--- modules/jenkins-fortify-slave/fortify-user-data.sh
+++ modules/jenkins-fortify-slave/fortify-user-data.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Install sca onto the host
+/home/ec2-user/run-sca.sh > /home/ec2-user/run-sca.out 2>&1
 
 # The following variables are replaced with terraform interpolation
-/home/ec2-user/run-sca-jenkins-node.sh ${jenkins_password} \
+/home/ec2-user/run-register-jenkins-node.sh ${jenkins_password} \
                                        ${jenkins_user} \
                                        ${jenkins_url} \
                                        ${fortify_ssh_password} \
@@ -11,4 +13,4 @@
                                        "${agent-description}" \
                                        ${agent-label-name} \
                                        ${credentials_id} \
-                                       > /home/ec2-user/run-sca-jenkins-node.out 2>&1
+                                       > /home/ec2-user/run-register-jenkins-node.out 2>&1
