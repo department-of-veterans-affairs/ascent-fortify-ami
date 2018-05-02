b103b3e83134c93a3c8ac961bf6e2c5ab09b3053
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-05-02 17:00:30 -0500

    PLAT-182: add symlink for javac and Java_home variable to bash_profile

--- packer/install-java.sh
+++ packer/install-java.sh
@@ -10,5 +10,12 @@ rm $JAVA_FILE.tar.gz
 sudo mkdir -p /usr/lib/jvm
 sudo mv $JAVA_FILE /usr/lib/jvm/$JAVA_FILE
 sudo chown -R root:root /usr/lib/jvm/$JAVA_FILE
-
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/$JAVA_FILE/bin/java 1200
+
+# Make a symlink for javac
+sudo ln -s /usr/lib/jvm/"'"$JAVA_FILE"'"/bin/javac /usr/bin/javac
+
+# Set JAVA_HOME environment variable
+sudo echo 'export JAVA_HOME=/usr/lib/jvm/"'"$JAVA_FILE"'"' | sudo tee -a /home/ec2-user/.bash_profile
+source /home/ec2-user/.bash_profile
+echo "JAVA_HOME=$JAVA_HOME"
