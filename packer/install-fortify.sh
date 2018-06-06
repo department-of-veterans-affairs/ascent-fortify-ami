00c37ab48bbb44b3f4a68c9941f7391ed4d2fdbe
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-06-06 12:21:58 -0500

    PLAT-177: Change the bucket name

--- packer/install-fortify.sh
+++ packer/install-fortify.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Download Fortify zip file from S3
-aws s3api --region us-gov-west-1 get-object --bucket ascent-fortify --key HPE_Security_Fortify_17.20_Server_WAR_Tomcat.zip  Fortify.zip
+aws s3api --region us-gov-west-1 get-object --bucket fortify-utility --key HPE_Security_Fortify_17.20_Server_WAR_Tomcat.zip  Fortify.zip
 
 # Download the Fortify SCA app from S3 and prep for install
-aws s3api --region us-gov-west-1 get-object --bucket ascent-fortify --key HPE_Security_Fortify_SCA_and_Apps_17.20_Linux.tar.gz Fortify_SCA.tar.gz
+aws s3api --region us-gov-west-1 get-object --bucket fortify-utility --key HPE_Security_Fortify_SCA_and_Apps_17.20_Linux.tar.gz Fortify_SCA.tar.gz
 sudo mkdir -p /opt/fortify_sca
 sudo tar -xzf /home/ec2-user/Fortify_SCA.tar.gz -C /opt/fortify_sca
 sudo mv fortify-sca.options /opt/fortify_sca/
 
 unzip -d Fortify Fortify.zip
-sudo cp Fortify/ssc.war /opt/tomcat/apache-tomcat-8.5.30/webapps/
+sudo cp Fortify/ssc.war /opt/tomcat/apache-tomcat-8.5.31/webapps/
 
 # Download connector and move to appropriate location
 wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.zip
 unzip mysql-connector-java-5.1.46.zip
 chmod 755 mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar
-sudo mv /home/ec2-user/setenv.sh /opt/tomcat/apache-tomcat-8.5.30/bin/
-sudo chmod 755 /opt/tomcat/apache-tomcat-8.5.30/bin/setenv.sh
+sudo mv /home/ec2-user/setenv.sh /opt/tomcat/apache-tomcat-8.5.31/bin/
+sudo chmod 755 /opt/tomcat/apache-tomcat-8.5.31/bin/setenv.sh
 
 # For some odd reason, this always errors out and says that tomcat is not running at localhost,
 # .. but it comes down anyway.
@@ -30,8 +30,8 @@ sudo /home/ec2-user/wait_for_tomcat_up.sh
 
 
 # Download Seed Bundles from S3
-aws s3api --region us-gov-west-1 get-object --bucket ascent-fortify --key HP_Fortify_Process_Seed_Bundle-2017_Q3.zip HP_Fortify_Process_Seed_Bundle-2017_Q3.zip
-aws s3api --region us-gov-west-1 get-object --bucket ascent-fortify --key HP_Fortify_Report_Seed_Bundle-2017_Q3.zip HP_Fortify_Report_Seed_Bundle-2017_Q3.zip
+aws s3api --region us-gov-west-1 get-object --bucket fortify-utility --key HP_Fortify_Process_Seed_Bundle-2017_Q3.zip HP_Fortify_Process_Seed_Bundle-2017_Q3.zip
+aws s3api --region us-gov-west-1 get-object --bucket fortify-utility --key HP_Fortify_Report_Seed_Bundle-2017_Q3.zip HP_Fortify_Report_Seed_Bundle-2017_Q3.zip
 
 
 # Make the run-*.sh scripts executable
