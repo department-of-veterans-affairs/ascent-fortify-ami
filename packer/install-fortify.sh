0b98a2cf89c8e0f4c214416d176d3affb4871e25
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-06-07 08:14:56 -0500

    PLAT-177: finish cleaning up SSC implementation as we're not going to use that

--- packer/install-fortify.sh
+++ packer/install-fortify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the Fortify SCA app from S3 and prep for install
-aws s3api --region us-gov-west-1 get-object --bucket fortify-utility --key HPE_Security_Fortify_SCA_and_Apps_17.20_Linux.tar.gz Fortify_SCA.tar.gz
+aws s3api --region us-gov-west-1 get-object --bucket ascent-fortify --key HPE_Security_Fortify_SCA_and_Apps_17.20_Linux.tar.gz Fortify_SCA.tar.gz
 sudo mkdir -p /opt/fortify_sca
 sudo tar -xzf /home/ec2-user/Fortify_SCA.tar.gz -C /opt/fortify_sca
 sudo mv fortify-sca.options /opt/fortify_sca/
