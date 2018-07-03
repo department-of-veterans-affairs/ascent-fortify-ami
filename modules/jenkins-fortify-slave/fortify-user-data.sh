01ee9763d3ea1787898a8cec17d1021d1cd00aff
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-07-03 08:55:26 -0500

    PLAT-177: put double quotes around everything in case there's a special character that can be misinterpreted as a bash command

--- modules/jenkins-fortify-slave/fortify-user-data.sh
+++ modules/jenkins-fortify-slave/fortify-user-data.sh
@@ -4,14 +4,14 @@
 /home/ec2-user/run-sca.sh > /home/ec2-user/run-sca.out 2>&1
 
 # The following variables are replaced with terraform interpolation
-/home/ec2-user/run-register-jenkins-node.sh ${jenkins_password} \
-                                       ${jenkins_user} \
-                                       ${jenkins_url} \
-                                       ${fortify_ssh_password} \
-                                       ${fortify_ssh_user} \
-                                       ${agent-name} \
+/home/ec2-user/run-register-jenkins-node.sh "${jenkins_password}" \
+                                       "${jenkins_user}" \
+                                       "${jenkins_url}" \
+                                       "${fortify_ssh_password}" \
+                                       "${fortify_ssh_user}" \
+                                       "${agent-name}" \
                                        "${agent-description}" \
-                                       ${agent-label-name} \
-                                       ${credentials_id} \
-                                       ${vault_url} \
+                                       "${agent-label-name}" \
+                                       "${credentials_id}" \
+                                       "${vault_url}" \
                                        > /home/ec2-user/run-register-jenkins-node.out 2>&1
