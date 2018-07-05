a77e4ffda3ba2d19746c4c2082ce9651b58744ba
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-07-05 16:04:07 -0500

    PLAt-177: add security group id as an output

--- modules/jenkins-fortify-slave/outputs.tf
+++ modules/jenkins-fortify-slave/outputs.tf
@@ -1,3 +1,8 @@
 output "jenkins_fortify_node_ip" {
   value = "${aws_instance.jenkins_fortify_node.private_ip}"
 }
+
+output "security_group_id" {
+  value = "${aws_security_group.fortify_security_group.id}"
+}
+
